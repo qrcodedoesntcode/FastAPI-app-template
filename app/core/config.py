8f15743dc6c38f875cf1e3bcd4481933bea62ccd
Antoine C <antoinechiris@gmail.com>
2021-08-08 19:47:34 +0200

    function _get for env

--- app/core/config.py
+++ app/core/config.py
@@ -1,25 +1,29 @@
 import os
+from typing import Optional
 
 
 class Config(object):
     """Base configuration."""
 
+    def _get(key: str, default: Optional[str] = ""):
+        return os.getenv(key, default)
+
     API_V1_STR = "/api/v1"
 
-    SECRET_KEY = os.getenvb(b"SECRET_KEY")
+    SECRET_KEY = _get("SECRET_KEY")
     if not SECRET_KEY:
         SECRET_KEY = os.urandom(32)
 
     ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 8  # 60 minutes * 24 hours * 8 days = 8 days
 
-    SERVER_NAME = os.getenv("SERVER_NAME")
-    SERVER_HOST = os.getenv("SERVER_HOST")
-    BACKEND_CORS_ORIGINS = os.getenv(
+    SERVER_NAME = _get("SERVER_NAME")
+    SERVER_HOST = _get("SERVER_HOST")
+    BACKEND_CORS_ORIGINS = _get(
         "BACKEND_CORS_ORIGINS"
     )  # a string of origins separated by commas, e.g: "http://localhost, http://localhost:4200, http://localhost:8080"
-    PROJECT_NAME = os.getenv("PROJECT_NAME", "FastAPI app template")
+    PROJECT_NAME = _get("PROJECT_NAME", "FastAPI app template")
 
-    DATABASE_USER = os.getenv("DATABASE_USER", "app")
-    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "password")
-    DATABASE_URL = os.getenv("DATABASE_URL", "127.0.0.1")
-    DATABASE_NAME = os.getenv("DATABASE_NAME", "app")
+    DATABASE_USER = _get("DATABASE_USER", "app")
+    DATABASE_PASSWORD = _get("DATABASE_PASSWORD", "password")
+    DATABASE_URL = _get("DATABASE_URL", "127.0.0.1")
+    DATABASE_NAME = _get("DATABASE_NAME", "app")
