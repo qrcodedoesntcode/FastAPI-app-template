4ae178c180609e82f9cc61294a2400dab6e3a592
Antoine C <antoinechiris@gmail.com>
2021-08-13 18:06:16 +0200

    Removed comments for the moment

--- app/models/mixins.py
+++ app/models/mixins.py
@@ -4,30 +4,18 @@ import sqlalchemy as sa
 
 
 class PrimaryKeyMixin:
-    """
-    Ajout d'une primary key "id" à toutes les entités
-    qui héritent de ce mixin
-    Ajoute aussi une méthode "get_by_id" pour récupérer
-    l'entité en question à partir de son id
-    """
 
     __abstract__ = True
     id = sa.Column(sa.Integer, primary_key=True)
 
 
 class TimestampsMixin:
-    """
-    Définition de deux champs "created_at" et "updated_at".
-        - created_at s'initialise une fois à l'insertion, et ne change plus après cela
-        - updated_at a la meme valeur que created_at à la création, puis à chaque mise à jour
-    de l'entité, le champs se met automatiquement à jour
-    """
 
     __abstract__ = True
 
     created_at = sa.Column(
         "created_at",
-        sa.TIMESTAMP(timezone=False),  # Pas supporté par mysql, mais au cas où
+        sa.TIMESTAMP(timezone=False),
         server_default=sa.text("now()"),
         default=datetime.utcnow,
         nullable=False,
@@ -35,7 +23,7 @@ class TimestampsMixin:
 
     updated_at = sa.Column(
         "updated_at",
-        sa.TIMESTAMP(timezone=False),  # Pas supporté par mysql, mais au cas où
+        sa.TIMESTAMP(timezone=False),
         server_default=sa.text("now()"),
         default=datetime.utcnow,
         onupdate=datetime.utcnow,
