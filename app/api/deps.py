91a5836ed8bcac0727c505540af2d2125f18a334
chichi13 <antoinechiris@gmail.com>
2021-11-01 21:09:03 +0100

    get_current_user in deps file

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,4 +1,15 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from sqlalchemy.orm import Session
+
+from app.core.config import Config
+from app.crud.user import get_user_by_username
 from app.models.database import SessionLocal
+from app.resources import strings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{Config.API_V1_STR}/login")
+
 
 def get_db():
     db = SessionLocal()
@@ -6,3 +17,28 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def get_current_user(
+    db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, Config.SECRET_KEY, algorithms=[Config.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = username
+    except jwt.JWTError:
+        raise credentials_exception
+    user = get_user_by_username(db, username=token_data)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=strings.USER_NOT_FOUND
+        )
+
+    return user
--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -1,46 +1,24 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 
-from app.core.config import Config
+from app import models
+from app.api import deps
 from app.crud.auth import check_email_is_taken, check_username_is_taken
-from app.crud.user import create_new_user, get_user_by_id, get_user_by_username
-from app.api.deps import get_db
+from app.crud.user import create_new_user, get_user_by_id
 from app.resources import strings
 from app.schemas.user import User, UserCreate
 
 router = APIRouter(prefix="/user")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{Config.API_V1_STR}/login")
 
 
 @router.get("/current", response_model=User, name="Get current user")
-def get_current_user(
-    db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)
-):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, Config.SECRET_KEY, algorithms=[Config.ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = username
-    except JWTError:
-        raise credentials_exception
-    user = get_user_by_username(db, username=token_data)
-    if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=strings.USER_NOT_FOUND
-        )
-    return user
+def get_current_user(current_user: models.User = Depends(deps.get_current_user)):
+
+    return current_user
 
 
 @router.post("/", name="Create a user", response_model=User)
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
+def create_user(user: UserCreate, db: Session = Depends(deps.get_db)):
     if check_username_is_taken(db, user.username):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -56,7 +34,7 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/{user_id}", response_model=User, name="Get specific user by id")
-def get_specific_user(user_id: int, db: Session = Depends(get_db)):
+def get_specific_user(user_id: int, db: Session = Depends(deps.get_db)):
     db_user = get_user_by_id(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(
