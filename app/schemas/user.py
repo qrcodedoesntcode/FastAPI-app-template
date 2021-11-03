203d38918485ec07131a2264475ce7e8a9832d6a
chichi13 <antoinechiris@gmail.com>
2021-11-03 23:43:21 +0100

    New user schema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,17 +1,35 @@
+from datetime import datetime
 from typing import Optional
 
 from pydantic import BaseModel, EmailStr
 
 
-class User(BaseModel):
+class UserBase(BaseModel):
     username: str
     email: EmailStr
     is_active: Optional[bool] = None
+
+
+class UserCreate(UserBase):
+    password: str
+    email: EmailStr
+
+
+class UserUpdate(UserBase):
+    pass
+
+
+class UserInDBBase(UserBase):
+    created_at: datetime = None
+    updated_at: datetime = None
 
     class Config:
         orm_mode = True
 
 
-class UserCreate(User):
+class User(UserInDBBase):
+    pass
+
+
+class UserInDB(UserInDBBase):
     password: str
-    email: EmailStr
